08ac215482cbc12f0322407be78c00233031bc8a
Wellington Aguiar <wellington@revelare.com.br>
2019-01-20 22:54:13 -0200

    dockerize | Dockerizing adminer

--- pgdb-create.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# PostgreSQL
-# Create DB
-docker-compose exec pgdb postgres psql --command="CREATE USER laravel-admin WITH PASSWORD '123456';"
-docker-compose exec pgdb postgres psql --command="CREATE DATABASE laravel-api OWNER laravel-admin;"
-
-#docker-compose exec pgdb postgres psql --dbname=laravel-api --command="CREATE EXTENSION adminpack; CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;"
