6aed4848ce95019f56356d721134a7de66c1ed7a
Wellington Aguiar <wellington@revelare.com.br>
2019-03-01 10:27:32 -0300

    users | Saving last login and IP during login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,23 +5,16 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\User;
 
 class LoginController extends Controller
 {
-    use AuthenticatesUsers;
-
-    public function authenticated(Request $request, $user)
-    {
-        $user->saveLastLogin($request->getClientIp());
-    }
-
     /**
      * Login creating auth data using laravel passport
      *
      * @return $tokenResponse
      */
-    public function login()
+    public function login(Request $request)
     {
         $createTokenRequest = Request::create(
             'oauth/token',
@@ -33,6 +26,10 @@ class LoginController extends Controller
         if (array_key_exists('error', $tokenResponse)) {
             return $this->sendError($tokenResponse->message, [], 401);
         }
+
+        User::where('email', $request->input('username'))
+            ->first()
+            ->saveLastLogin($request->getClientIp());
 
         return $this->sendResponse($tokenResponse, 'Login success');
     }
